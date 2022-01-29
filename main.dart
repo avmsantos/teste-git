116e88aa88f074386fb726aef97725350010cee8
Amanda Victoria <amandamelquiades01@gmail.com>
2022-01-29 16:57:47 -0300

    funcao adicao

--- main.dart
+++ main.dart
@@ -1,3 +1,27 @@
-void  main() {
-  
+import 'dart:io';
+
+main() {
+  /////////   sosma
+  print("Digite um valor");
+  double? primeiroValor = double.parse(stdin.readLineSync()!);
+  print("Digite outro valor");
+  double? segundoValor = double.parse(stdin.readLineSync()!);
+  //print("adicao");
+
+  final operacao = stdin.readLineSync()!;
+
+  var resultado = 0.0;
+
+  switch (operacao) {
+    case 'adicao':
+      resultado = adicao(primeiroValor, segundoValor);
+      break;
+  }
+
+  print(resultado);
+}
+
+
+double adicao(double a, double b) {
+  return a + b;
 }
